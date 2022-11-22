6f4b04981ffbb6faa3037a393b07cc5c1abf144b
Miguel Lopez Pedres <lopezpedres@gmail.com>
2022-11-22 10:20:21 -0700

    Added WarehouseList Component

---
+++ src/components/WarehouseItem/WarehouseItem.js
@@ -0,0 +1,46 @@
+import "./WarehouseItem.scss";
+import React from "react";
+import { Link } from "react-router-dom";
+import deleteIcon from "../../assets/icons/delete_outline-24px.svg";
+import editIcon from "../../assets/icons/edit-24px.svg";
+
+const WarehouseItem = () => {
+    return (
+        <li className="warehouse">
+            <section className="warehouse__info">
+                <ul className="warehouse__list-items">
+                    <li className="warehouse__item">
+                        <span className="warehouse__item-label">WAREHOUSE</span>
+                        <a className="warehouse__link">Manhattan { ">" }</a>
+                    </li>
+                    <li className="warehouse__item">
+                        <span className="warehouse__item-label">ADDRESS</span>
+                        <span className="warehouse__item-value">503 Broadway, New York, USA</span>
+                    </li>
+
+                </ul>
+                <ul className="warehouse__list-items">
+                    <li className="warehouse__item">
+                        <span className="warehouse__item-label">CONTACT NAME</span>
+                        <span>Parmin Aujla</span>
+                    </li>
+                    <li className="warehouse__item">
+                        <span className="warehouse__item-label">CONTACT INFORMATION</span>
+                        <span>+1 (629) 555-0129 <br />
+                            paujla@instock.com</span>
+                    </li>
+                </ul>
+            </section>
+            <div className=" warehouse__icons-container">
+                <a>
+                    <img className=" warehouse__icons" src={ deleteIcon } alt="" />
+                </a>
+                <a>
+                    <img className=" warehouse__icons" src={ editIcon } alt="" />
+                </a>
+            </div>
+        </li>
+    );
+};
+
+export default WarehouseItem;
