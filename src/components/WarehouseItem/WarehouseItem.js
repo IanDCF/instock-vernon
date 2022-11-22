91869319ff49bf9411a0a28836b5eb2cdccc2965
Miguel Lopez Pedres <lopezpedres@gmail.com>
2022-11-22 16:18:06 -0700

    Added chevron icon

--- src/components/WarehouseItem/WarehouseItem.js
+++ src/components/WarehouseItem/WarehouseItem.js
@@ -3,6 +3,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import deleteIcon from "../../assets/icons/delete_outline-24px.svg";
 import editIcon from "../../assets/icons/edit-24px.svg";
+import chevronIcon from "../../assets/icons/chevron_right-24px.svg";
 
 const WarehouseItem = () => {
     return (
@@ -11,13 +12,17 @@ const WarehouseItem = () => {
                 <ul className="warehouse__list-items">
                     <li className="warehouse__item">
                         <span className="warehouse__item-label">WAREHOUSE</span>
-                        <a className="warehouse__link">Manhattan { ">" }</a>
+                        <a className="warehouse__link">
+                            <span>Manhattan</span>
+                            <img src={ chevronIcon } alt="" />
+                        </a>
                     </li>
                     <li className="warehouse__item">
                         <span className="warehouse__item-label">ADDRESS</span>
-                        <span className="warehouse__item-value">503 Broadway, New York, USA</span>
+                        <span className="warehouse__item-value">
+                            503 Broadway, New York, USA
+                        </span>
                     </li>
-
                 </ul>
                 <ul className="warehouse__list-items">
                     <li className="warehouse__item">
@@ -26,16 +31,18 @@ const WarehouseItem = () => {
                     </li>
                     <li className="warehouse__item">
                         <span className="warehouse__item-label">CONTACT INFORMATION</span>
-                        <span>+1 (629) 555-0129 <br />
-                            paujla@instock.com</span>
+                        <span>
+                            +1 (629) 555-0129 <br />
+                            paujla@instock.com
+                        </span>
                     </li>
                 </ul>
             </section>
             <div className=" warehouse__icons-container">
-                <a>
+                <a className="warehouse__delete">
                     <img className=" warehouse__icons" src={ deleteIcon } alt="" />
                 </a>
-                <a>
+                <a className="warehouse__edit">
                     <img className=" warehouse__icons" src={ editIcon } alt="" />
                 </a>
             </div>
