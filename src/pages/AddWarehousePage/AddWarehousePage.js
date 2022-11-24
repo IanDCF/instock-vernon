21091135ab474ff05d8ff60ddb9531f4549959d1
Lucas Lu <lucaslu@hotmail.com>
2022-11-23 23:59:26 -0800

    add validations to all fields

--- src/pages/AddWarehousePage/AddWarehousePage.js
+++ src/pages/AddWarehousePage/AddWarehousePage.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 
 import arrowBackIcon from "../../assets/icons/arrow_back-24px.svg";
+import errorIcon from "../../assets/icons/error-24px.svg";
 import "./AddWarehousePage.scss";
 
 const AddWarehousePage = () => {
@@ -15,6 +16,15 @@ const AddWarehousePage = () => {
   const [phone, setPhone] = useState("");
   const [email, setEmail] = useState("");
 
+  const [isWarehouse, setIsWarehouse] = useState(true);
+  const [isAddress, setIsAddress] = useState(true);
+  const [isCity, setIsCity] = useState(true);
+  const [isCountry, setIsCountry] = useState(true);
+  const [isContact, setIsContact] = useState(true);
+  const [isPosition, setIsPosition] = useState(true);
+  const [isPhone, setIsPhone] = useState(true);
+  const [isEmail, setIsEmail] = useState(true);
+
   const handleChangeWarehouse = (event) => setWarehouseName(event.target.value);
   const handleChangeAddress = (event) => setAddress(event.target.value);
   const handleChangeCity = (event) => setCity(event.target.value);
@@ -25,6 +35,128 @@ const AddWarehousePage = () => {
   const handleChangeEmail = (event) => setEmail(event.target.value);
 
   const BACKEND = "http://localhost:8080";
+
+  const isWarehouseValid = () => {
+    if (warehouseName.length === 0) {
+      setIsWarehouse(false);
+      return false;
+    }
+    setIsWarehouse(true);
+    return true;
+  };
+
+  const isAddressValid = () => {
+    if (address.length === 0) {
+      setIsAddress(false);
+      return false;
+    }
+    setIsAddress(true);
+    return true;
+  };
+
+  const isCityValid = () => {
+    if (city.length === 0) {
+      setIsCity(false);
+      return false;
+    }
+    setIsCity(true);
+    return true;
+  };
+
+  const isCountryValid = () => {
+    if (country.length === 0) {
+      setIsCountry(false);
+      return false;
+    }
+    setIsCountry(true);
+    return true;
+  };
+
+  const isContactValid = () => {
+    if (contactName.length === 0) {
+      setIsContact(false);
+      return false;
+    }
+    setIsContact(true);
+    return true;
+  };
+
+  const isPositionValid = () => {
+    if (position.length === 0) {
+      setIsPosition(false);
+      return false;
+    }
+    setIsPosition(true);
+    return true;
+  };
+
+  const validatePhoneNumber = (input_str) => {
+    let re = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
+    return re.test(input_str);
+  };
+
+  const isPhoneValid = () => {
+    if (!validatePhoneNumber(phone) || phone.length === 0) {
+      setIsPhone(false);
+      return false;
+    }
+    setIsPhone(true);
+    return true;
+  };
+
+  const checkEmail = (email) => {
+    return /\S+@\S+\.\S+/.test(email);
+  };
+
+  const isEmailValid = () => {
+    if (!checkEmail(email) || email.length === 0) {
+      setIsEmail(false);
+      return false;
+    }
+    setIsEmail(true);
+    return true;
+  };
+
+  const isFormValid = () => {
+    let flag = true;
+
+    if (!isWarehouseValid()) {
+      flag = false;
+    }
+
+    if (!isAddressValid()) {
+      flag = false;
+    }
+
+    if (!isCityValid()) {
+      flag = false;
+    }
+
+    if (!isCountryValid()) {
+      flag = false;
+    }
+
+    if (!isContactValid()) {
+      flag = false;
+    }
+
+    if (!isPositionValid()) {
+      flag = false;
+    }
+
+    if (!isPhoneValid()) {
+      flag = false;
+    }
+
+    if (!isEmailValid()) {
+      flag = false;
+    }
+
+    if (flag === false) return false;
+
+    console.log("Form valid");
+    return true;
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -40,19 +172,24 @@ const AddWarehousePage = () => {
       contact_email: email,
     };
 
-    axios
-      .post(`${BACKEND}/warehouses`, warehouseObj)
-      .then(() => {
-        setWarehouseName("");
-        setAddress("");
-        setCity("");
-        setCountry("");
-        setContactName("");
-        setPosition("");
-        setPhone("");
-        setEmail("");
-      })
-      .catch((error) => console.log(error));
+    if (isFormValid()) {
+      console.log("Form Valid on submit");
+      axios
+        .post(`${BACKEND}/warehouses`, warehouseObj)
+        .then(() => {
+          setWarehouseName("");
+          setAddress("");
+          setCity("");
+          setCountry("");
+          setContactName("");
+          setPosition("");
+          setPhone("");
+          setEmail("");
+        })
+        .catch((error) => console.log(error));
+    } else {
+      return;
+    }
   };
 
   return (
@@ -73,46 +210,96 @@ const AddWarehousePage = () => {
                   Warehouse Name
                 </label>
                 <input
-                  className="add-wh__input"
+                  className={`add-wh__input ${
+                    !isWarehouse && "add-wh__input--required"
+                  }`}
                   type="text"
                   name="warehouse-name"
                   placeholder="Warehouse Name"
                   value={warehouseName}
                   onChange={handleChangeWarehouse}
                 />
+                {!isWarehouse && (
+                  <p className="add-wh__error">
+                    <img
+                      className="add-wh__error-img"
+                      src={errorIcon}
+                      alt="Error"
+                    />
+                    This field is required
+                  </p>
+                )}
                 <label className="add-wh__label" htmlFor="address">
                   Street Address
                 </label>
                 <input
-                  className="add-wh__input"
+                  className={`add-wh__input ${
+                    !isAddress && "add-wh__input--required"
+                  }`}
                   type="text"
                   name="address"
                   placeholder="Street Address"
                   value={address}
                   onChange={handleChangeAddress}
                 />
+                {!isAddress && (
+                  <p className="add-wh__error">
+                    <img
+                      className="add-wh__error-img"
+                      src={errorIcon}
+                      alt="Error"
+                    />
+                    This field is required
+                  </p>
+                )}
+
                 <label className="add-wh__label" htmlFor="city">
                   City
                 </label>
                 <input
-                  className="add-wh__input"
+                  className={`add-wh__input ${
+                    !isCity && "add-wh__input--required"
+                  }`}
                   type="text"
                   name="city"
                   placeholder="City"
                   value={city}
                   onChange={handleChangeCity}
                 />
+                {!isCity && (
+                  <p className="add-wh__error">
+                    <img
+                      className="add-wh__error-img"
+                      src={errorIcon}
+                      alt="Error"
+                    />
+                    This field is required
+                  </p>
+                )}
+
                 <label className="add-wh__label" htmlFor="country">
                   Country
                 </label>
                 <input
-                  className="add-wh__input"
+                  className={`add-wh__input ${
+                    !isCountry && "add-wh__input--required"
+                  }`}
                   type="text"
                   name="country"
                   placeholder="Country"
                   value={country}
                   onChange={handleChangeCountry}
                 />
+                {!isCountry && (
+                  <p className="add-wh__error">
+                    <img
+                      className="add-wh__error-img"
+                      src={errorIcon}
+                      alt="Error"
+                    />
+                    This field is required
+                  </p>
+                )}
               </div>
               <div className="add-wh__details-wrapper">
                 <h2 className="add-wh__subtitle">Contact Details</h2>
@@ -120,46 +307,94 @@ const AddWarehousePage = () => {
                   Contact Name
                 </label>
                 <input
-                  className="add-wh__input"
+                  className={`add-wh__input ${
+                    !isContact && "add-wh__input--required"
+                  }`}
                   type="text"
                   name="contact-name"
                   placeholder="Contact Name"
                   value={contactName}
                   onChange={handleChangeContact}
                 />
+                {!isContact && (
+                  <p className="add-wh__error">
+                    <img
+                      className="add-wh__error-img"
+                      src={errorIcon}
+                      alt="Error"
+                    />
+                    This field is required
+                  </p>
+                )}
                 <label className="add-wh__label" htmlFor="position">
                   Position
                 </label>
                 <input
-                  className="add-wh__input"
+                  className={`add-wh__input ${
+                    !isPosition && "add-wh__input--required"
+                  }`}
                   type="text"
                   name="position"
                   placeholder="Position"
                   value={position}
                   onChange={handleChangePosition}
                 />
+                {!isPosition && (
+                  <p className="add-wh__error">
+                    <img
+                      className="add-wh__error-img"
+                      src={errorIcon}
+                      alt="Error"
+                    />
+                    This field is required
+                  </p>
+                )}
                 <label className="add-wh__label" htmlFor="phone">
                   Phone Number
                 </label>
                 <input
-                  className="add-wh__input"
+                  className={`add-wh__input ${
+                    !isPhone && "add-wh__input--required"
+                  }`}
                   type="text"
                   name="phone"
                   placeholder="Phone Number"
                   value={phone}
                   onChange={handleChangePhone}
                 />
+                {!isPhone && (
+                  <p className="add-wh__error">
+                    <img
+                      className="add-wh__error-img"
+                      src={errorIcon}
+                      alt="Error"
+                    />
+                    This field is required
+                  </p>
+                )}
                 <label className="add-wh__label" htmlFor="email">
                   Email
                 </label>
                 <input
-                  className="add-wh__input"
+                  className={`add-wh__input ${
+                    !isEmail && "add-wh__input--required"
+                  }`}
                   type="text"
                   name="email"
                   placeholder="Email"
                   value={email}
                   onChange={handleChangeEmail}
                 />
+                {!isEmail && (
+                  <p className="add-wh__error">
+                    <img
+                      className="add-wh__error-img"
+                      src={errorIcon}
+                      alt="Error"
+                    />
+                    This field is required
+                  </p>
+                )}
               </div>
             </div>
             <div className="add-wh__buttons-wrapper">
