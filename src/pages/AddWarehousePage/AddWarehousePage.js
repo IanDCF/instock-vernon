8dd5a5aa7082f13fd082fb61653f810e65cfcb02
Lucas Lu <lucaslu@hotmail.com>
2022-11-23 19:58:51 -0800

    add Add New Warehouse Component Desktop version and axios POST

--- src/pages/AddWarehousePage/AddWarehousePage.js
+++ src/pages/AddWarehousePage/AddWarehousePage.js
@@ -1,8 +1,60 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+
 import arrowBackIcon from "../../assets/icons/arrow_back-24px.svg";
 import "./AddWarehousePage.scss";
 
 const AddWarehousePage = () => {
+  const [warehouseName, setWarehouseName] = useState("");
+  const [address, setAddress] = useState("");
+  const [city, setCity] = useState("");
+  const [country, setCountry] = useState("");
+  const [contactName, setContactName] = useState("");
+  const [position, setPosition] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleChangeWarehouse = (event) => setWarehouseName(event.target.value);
+  const handleChangeAddress = (event) => setAddress(event.target.value);
+  const handleChangeCity = (event) => setCity(event.target.value);
+  const handleChangeCountry = (event) => setCountry(event.target.value);
+  const handleChangeContact = (event) => setContactName(event.target.value);
+  const handleChangePosition = (event) => setPosition(event.target.value);
+  const handleChangePhone = (event) => setPhone(event.target.value);
+  const handleChangeEmail = (event) => setEmail(event.target.value);
+
+  const BACKEND = "http://localhost:8080";
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const warehouseObj = {
+      warehouse_name: warehouseName,
+      address,
+      city,
+      country,
+      contact_name: contactName,
+      contact_position: position,
+      contact_phone: phone,
+      contact_email: email,
+    };
+
+    axios
+      .post(`${BACKEND}/warehouses`, warehouseObj)
+      .then(() => {
+        setWarehouseName("");
+        setAddress("");
+        setCity("");
+        setCountry("");
+        setContactName("");
+        setPosition("");
+        setPhone("");
+        setEmail("");
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
     <section className="add-wh">
       <div className="add-wh__wrapper">
@@ -13,7 +65,7 @@ const AddWarehousePage = () => {
             </Link>
             <h1 className="add-wh__title">Add New Warehouse</h1>
           </div>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="add-wh__form-wrapper">
               <div className="add-wh__details-wrapper">
                 <h2 className="add-wh__subtitle">Warehouse Details</h2>
@@ -25,6 +77,8 @@ const AddWarehousePage = () => {
                   type="text"
                   name="warehouse-name"
                   placeholder="Warehouse Name"
+                  value={warehouseName}
+                  onChange={handleChangeWarehouse}
                 />
                 <label className="add-wh__label" htmlFor="address">
                   Street Address
@@ -34,6 +88,8 @@ const AddWarehousePage = () => {
                   type="text"
                   name="address"
                   placeholder="Street Address"
+                  value={address}
+                  onChange={handleChangeAddress}
                 />
                 <label className="add-wh__label" htmlFor="city">
                   City
@@ -43,15 +99,19 @@ const AddWarehousePage = () => {
                   type="text"
                   name="city"
                   placeholder="City"
+                  value={city}
+                  onChange={handleChangeCity}
                 />
                 <label className="add-wh__label" htmlFor="country">
-                  City
+                  Country
                 </label>
                 <input
                   className="add-wh__input"
                   type="text"
                   name="country"
                   placeholder="Country"
+                  value={country}
+                  onChange={handleChangeCountry}
                 />
               </div>
               <div className="add-wh__details-wrapper">
@@ -64,6 +124,8 @@ const AddWarehousePage = () => {
                   type="text"
                   name="contact-name"
                   placeholder="Contact Name"
+                  value={contactName}
+                  onChange={handleChangeContact}
                 />
                 <label className="add-wh__label" htmlFor="position">
                   Position
@@ -73,6 +135,8 @@ const AddWarehousePage = () => {
                   type="text"
                   name="position"
                   placeholder="Position"
+                  value={position}
+                  onChange={handleChangePosition}
                 />
                 <label className="add-wh__label" htmlFor="phone">
                   Phone Number
@@ -82,6 +146,8 @@ const AddWarehousePage = () => {
                   type="text"
                   name="phone"
                   placeholder="Phone Number"
+                  value={phone}
+                  onChange={handleChangePhone}
                 />
                 <label className="add-wh__label" htmlFor="email">
                   Email
@@ -91,12 +157,18 @@ const AddWarehousePage = () => {
                   type="text"
                   name="email"
                   placeholder="Email"
+                  value={email}
+                  onChange={handleChangeEmail}
                 />
               </div>
             </div>
             <div className="add-wh__buttons-wrapper">
-              <button className="add-wh__cancel-button">Cancel</button>
-              <button className="add-wh__add-button">+ Add Warehouse</button>
+              <Link to="/warehouse">
+                <button className="add-wh__cancel-button">Cancel</button>
+              </Link>
+              <button className="add-wh__add-button" type="submit">
+                + Add Warehouse
+              </button>
             </div>
           </form>
         </div>
