0f84fbd6fb70eb2110cc5d122100821c3b54a33a
Lucas Lu <lucaslu@hotmail.com>
2022-11-23 18:07:24 -0800

    add Add New Warehouse Component Tablet Version

--- src/pages/AddWarehousePage/AddWarehousePage.js
+++ src/pages/AddWarehousePage/AddWarehousePage.js
@@ -14,87 +14,89 @@ const AddWarehousePage = () => {
             <h1 className="add-wh__title">Add New Warehouse</h1>
           </div>
           <form>
-            <div className="add-wh__details-wrapper">
-              <h2 className="add-wh__subtitle">Warehouse Details</h2>
-              <label className="add-wh__label" htmlFor="warehouse-name">
-                Warehouse Name
-              </label>
-              <input
-                className="add-wh__input"
-                type="text"
-                name="warehouse-name"
-                placeholder="Warehouse Name"
-              />
-              <label className="add-wh__label" htmlFor="address">
-                Street Address
-              </label>
-              <input
-                className="add-wh__input"
-                type="text"
-                name="address"
-                placeholder="Street Address"
-              />
-              <label className="add-wh__label" htmlFor="city">
-                City
-              </label>
-              <input
-                className="add-wh__input"
-                type="text"
-                name="city"
-                placeholder="City"
-              />
-              <label className="add-wh__label" htmlFor="country">
-                City
-              </label>
-              <input
-                className="add-wh__input"
-                type="text"
-                name="country"
-                placeholder="Country"
-              />
-            </div>
-            <div className="add-wh__details-wrapper">
-              <h2 className="add-wh__subtitle">Contact Details</h2>
-              <label className="add-wh__label" htmlFor="contact-name">
-                Contact Name
-              </label>
-              <input
-                className="add-wh__input"
-                type="text"
-                name="contact-name"
-                placeholder="Contact Name"
-              />
-              <label className="add-wh__label" htmlFor="position">
-                Position
-              </label>
-              <input
-                className="add-wh__input"
-                type="text"
-                name="position"
-                placeholder="Position"
-              />
-              <label className="add-wh__label" htmlFor="phone">
-                Phone Number
-              </label>
-              <input
-                className="add-wh__input"
-                type="text"
-                name="phone"
-                placeholder="Phone Number"
-              />
-              <label className="add-wh__label" htmlFor="email">
-                Email
-              </label>
-              <input
-                className="add-wh__input"
-                type="text"
-                name="email"
-                placeholder="Email"
-              />
-              <div className="add-wh__buttons-wrapper">
-                <button className="add-wh__cancel-button">Cancel</button>
-                <button className="add-wh__add-button">+ Add Warehouse</button>
+            <div className="add-wh__form-wrapper">
+              <div className="add-wh__details-wrapper">
+                <h2 className="add-wh__subtitle">Warehouse Details</h2>
+                <label className="add-wh__label" htmlFor="warehouse-name">
+                  Warehouse Name
+                </label>
+                <input
+                  className="add-wh__input"
+                  type="text"
+                  name="warehouse-name"
+                  placeholder="Warehouse Name"
+                />
+                <label className="add-wh__label" htmlFor="address">
+                  Street Address
+                </label>
+                <input
+                  className="add-wh__input"
+                  type="text"
+                  name="address"
+                  placeholder="Street Address"
+                />
+                <label className="add-wh__label" htmlFor="city">
+                  City
+                </label>
+                <input
+                  className="add-wh__input"
+                  type="text"
+                  name="city"
+                  placeholder="City"
+                />
+                <label className="add-wh__label" htmlFor="country">
+                  City
+                </label>
+                <input
+                  className="add-wh__input"
+                  type="text"
+                  name="country"
+                  placeholder="Country"
+                />
               </div>
+              <div className="add-wh__details-wrapper">
+                <h2 className="add-wh__subtitle">Contact Details</h2>
+                <label className="add-wh__label" htmlFor="contact-name">
+                  Contact Name
+                </label>
+                <input
+                  className="add-wh__input"
+                  type="text"
+                  name="contact-name"
+                  placeholder="Contact Name"
+                />
+                <label className="add-wh__label" htmlFor="position">
+                  Position
+                </label>
+                <input
+                  className="add-wh__input"
+                  type="text"
+                  name="position"
+                  placeholder="Position"
+                />
+                <label className="add-wh__label" htmlFor="phone">
+                  Phone Number
+                </label>
+                <input
+                  className="add-wh__input"
+                  type="text"
+                  name="phone"
+                  placeholder="Phone Number"
+                />
+                <label className="add-wh__label" htmlFor="email">
+                  Email
+                </label>
+                <input
+                  className="add-wh__input"
+                  type="text"
+                  name="email"
+                  placeholder="Email"
+                />
+              </div>
+            </div>
+            <div className="add-wh__buttons-wrapper">
+              <button className="add-wh__cancel-button">Cancel</button>
+              <button className="add-wh__add-button">+ Add Warehouse</button>
             </div>
           </form>
         </div>
