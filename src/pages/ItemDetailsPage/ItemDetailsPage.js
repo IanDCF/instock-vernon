7114bd878b942894aadfb55470af260b6f579c0b
jaypostle <jaysondigitalmcc@gmail.com>
2022-11-28 15:53:49 -0800

    inventory item page, make sure edit goes to edit that item

--- src/pages/ItemDetailsPage/ItemDetailsPage.js
+++ src/pages/ItemDetailsPage/ItemDetailsPage.js
@@ -1,12 +1,12 @@
 import "./ItemDetailsPage.scss";
 import ArrowBack from "../../assets/icons/arrow_back-24px.svg";
 import EditButton from "../../components/Buttons/EditButton/EditButton";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
 import InventoryTagInStock from "../../components/Buttons/InventoryTag/InventoryTagInStock";
 import InventoryTagOutOfStock from "../../components/Buttons/InventoryTag/InventoryTagOutOfStock";
 import { useEffect, useState } from "react";
 import { getInventoryItem } from "../../utils/utils";
-import PageWrapper from '../../components/PageWrapper/PageWrapper'
+import PageWrapper from "../../components/PageWrapper/PageWrapper";
 const ItemDetailsPage = () => {
   const { itemId } = useParams();
   const [inventoryItem, setInventoryItem] = useState();
@@ -17,26 +17,33 @@ const ItemDetailsPage = () => {
     };
     fetchInventoryItem();
   }, [itemId]);
+
+  let navigate = useNavigate();
+
+  const navigateToEdit = () => {
+    navigate(`/inventory/${itemId}/edit`);
+  };
+
   return (
     <PageWrapper>
       <section className="item-details">
-        { inventoryItem && (
+        {inventoryItem && (
           <>
             <article className="item-details__title-wrapper">
               <div className="item-details__title">
                 <section className="item-details__back-title-wrapper">
-                  <Link to={ "/inventory" }>
+                  <Link to={"/inventory"}>
                     <img
                       className="item-details__back-arrow"
-                      src={ ArrowBack }
+                      src={ArrowBack}
                       alt="back arrow"
                     />
                   </Link>
                   <h1 className="item-details__title-value">
-                    { inventoryItem.item_name }
+                    {inventoryItem.item_name}
                   </h1>
                 </section>
-                <Link to={ `/inventory/${inventoryItem.id}/edit` }>
+                <Link to={`/inventory/${inventoryItem.id}/edit`}>
                   <EditButton />
                 </Link>
               </div>
@@ -46,13 +53,13 @@ const ItemDetailsPage = () => {
                 <div>
                   <h5 className="item-details__label">ITEM DESCRIPTION:</h5>
                   <p className="item-details__description">
-                    { inventoryItem.description }
+                    {inventoryItem.description}
                   </p>
                 </div>
                 <div>
                   <h5 className="item-details__label">CATEGORY:</h5>
                   <p className="item-details__description">
-                    { inventoryItem.category }
+                    {inventoryItem.category}
                   </p>
                 </div>
               </section>
@@ -60,29 +67,29 @@ const ItemDetailsPage = () => {
                 <article className="item-details__status-quantity">
                   <div className="item-details__item-wrapper item-details__item-wrapper--status">
                     <h5 className="item-details__label">STATUS:</h5>
-                    { inventoryItem.satus === "In Stock" ? (
+                    {inventoryItem.satus === "In Stock" ? (
                       <InventoryTagInStock />
                     ) : (
                       <InventoryTagOutOfStock />
-                    ) }
+                    )}
                   </div>
                   <div>
                     <h5 className="item-details__label">QUANTITY</h5>
                     <p className="item-details__description">
-                      { inventoryItem.quantity }
+                      {inventoryItem.quantity}
                     </p>
                   </div>
                 </article>
                 <div>
                   <h5 className="item-details__label">WAREHOUSE:</h5>
                   <p className="item-details__description">
-                    { inventoryItem.warehouse_name }
+                    {inventoryItem.warehouse_name}
                   </p>
                 </div>
               </section>
             </article>
           </>
-        ) }
+        )}
       </section>
     </PageWrapper>
   );
